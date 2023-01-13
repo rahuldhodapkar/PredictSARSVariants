96d05c86fb0e47e5d42452ba7bc237e7a42cabb5
rahuldhodapkar <rahul.dhodapkar@yale.edu>
2023-01-12 22:04:22 -0500

    add mutation density plots

--- src/manuscript_figures.R
+++ src/manuscript_figures.R
@@ -20,9 +20,7 @@ s <- ifelse(!dir.exists('./calc'), dir.create('./calc'), FALSE)
 # Load Data
 ################################################################################
 
-df <- read.csv('./calc/variant_meta_df.csv')
-df$location <- sapply(df$Mutation, function(x) { 
-  as.numeric(substr(x, 2, str_length(x)-1)) })
+
 
 data('BLOSUM80')
 data('BLOSUM100')
@@ -37,40 +35,38 @@ data('PAM250')
 ## Show Density Plot of Substitution Locations
 ###############################################
 
-df.sub <- subset(df, location >= 438 & location <= 506)
-
-plot.df <- data.frame(
-  group = c(rep('Train', sum(df.sub$in_train == 'True')),
-            rep('Test', sum(df.sub$in_test == 'True')),
-            rep('Predict', sum(df.sub$in_predict == 'True'))),
-  location = c(
-    df.sub$location[df.sub$in_train == 'True'],
-    df.sub$location[df.sub$in_test == 'True'],
-    df.sub$location[df.sub$in_predict == 'True']
-  )
-)
-
-ggplot(plot.df, aes(x=location, fill=group)) + 
-  geom_density(alpha=0.4, adjust=0.1) +
-  scale_fill_manual(values=c('#648fff', '#785ef0', '#dc267f')) +
-  xlim(c(438, 506)) +
-  facet_grid(group ~ .) +
-  theme_cowplot() + 
-  background_grid(minor='x')
-
-
 df <- read.csv('./calc/merged_substitution_info.csv')
 df$location <- sapply(df$substitution, function(x) { 
   as.numeric(substr(x, 2, str_length(x)-1)) })
+df$group <- factor(df$group, levels=c('Train', 'Test', 'Predict'), ordered=T)
+
+plt.mutation.density <- ggplot(df, aes(x=location, fill=group, weight=log10(frequency))) + 
+  geom_density(adjust=0.1) +
+  scale_fill_manual(values=c('#648fff', '#785ef0', '#dc267f')) +
+  facet_grid(group ~ .) +
+  theme_cowplot() + 
+  scale_x_continuous(
+    breaks=seq(from=440, to=506, by=5),
+    minor_breaks=seq(from=438, to=506, by=1),
+    limits=c(438, 506)
+  ) +
+  background_grid(minor='x')
+ggsave('./fig/mutation_density_plot.png', width=12, height=5)
 
 ggplot(df, aes(x=location, fill=group, weight=log10(frequency))) + 
   geom_density(alpha=0.4, adjust=0.1) +
+  geom_histogram(aes(y = stat(density)), color='#000000', alpha=0.4, binwidth = 1) +
   scale_fill_manual(values=c('#648fff', '#785ef0', '#dc267f')) +
-  xlim(c(438, 506)) +
   facet_grid(group ~ .) +
   theme_cowplot() + 
+  scale_x_continuous(
+    breaks=seq(from=440, to=506, by=5),
+    minor_breaks=seq(from=438, to=506, by=1),
+    limits=c(438, 506)
+  ) +
   background_grid(minor='x')
-ggsave('./fig/mutation_density_plot.png', width=12, height=5)
+ggsave('./fig/mutation_histogram_plot.png', width=12, height=5)
+
 
 ###############################################
 ## Compare using PAM and BLOSUM
@@ -112,7 +108,6 @@ plot.df$group <- factor(plot.df$group,
                         levels=c('Random', 'Train', 'Test', 'Predict'),
                         ordered=T)
 
-
 #
 # Make Plots
 #
@@ -136,4 +131,42 @@ ggsave('./fig/pam30_boxplot.png', width=5, height=4)
 wilcox.test(pam30 ~ group, data=subset(plot.df, group %in% c('Predict', 'Random')))
 wilcox.test(pam30 ~ group, data=subset(plot.df, group %in% c('Train', 'Random')))
 wilcox.test(pam30 ~ group, data=subset(plot.df, group %in% c('Test', 'Random')))
+
+################################################################################
+# Create Pie / Donut Charts
+################################################################################
+df <- read.csv('./calc/merged_substitution_info.csv')
+df$location <- sapply(df$substitution, function(x) { 
+  as.numeric(substr(x, 2, str_length(x)-1)) })
+
+sub.venn.df <- as.data.frame.matrix(table(df$substitution, df$group))
+
+sub.venn.df[sub.venn.df$Predict == 1
+            & sub.venn.df$Train == 0
+            & sub.venn.df$Test == 1,]
+sub.venn.df$ID <- paste0(sub.venn.df$Predict, sub.venn.df$Train, sub.venn.df$Test)
+
+predict.df <- subset(df, group == 'Predict')
+predict.df$ID <- sub.venn.df$ID[match(predict.df$substitution, rownames(sub.venn.df))]
+predict.df$ID <- factor(predict.df$ID, levels=c('101', '110', '111', '100'), 
+                        ordered=T)
+
+plt.pred.identities <- ggplot(predict.df, aes(x=location, fill=ID)) +
+  geom_histogram(binwidth = 1, color='#000000', size=0.5) +
+  scale_fill_manual(values=c('#785ef0', '#648fff', '#fe6100', '#dc267f')) +
+  scale_x_continuous(
+    breaks=seq(from=440, to=506, by=5),
+    minor_breaks=seq(from=438, to=506, by=1),
+    limits=c(438, 506)
+  ) +
+  theme_cowplot() + 
+  background_grid(minor='xy')
+ggsave('./fig/prediction_identity_plot.png', width=12, height=2)
+
+plt.merged <- plot_grid(
+  plt.mutation.density + theme(axis.title.x = element_blank(), axis.text.x = element_blank()),
+  plt.pred.identities,
+  ncol=1, align='v', axis='rl', rel_heights = c(3.4,1))
+ggsave('./fig/merged_substitution_plots.png', width=12, height=7.3)
+
 
